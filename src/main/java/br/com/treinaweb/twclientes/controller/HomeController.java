8859b46791b90ce3c740a53a0db7229cc60393c2
Cleyson Lima <cleysonph@gmail.com>
2021-03-09 23:57:30 -0300

    04-05: th:text, th:utext e inlined expressions

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -1,6 +1,10 @@
 package br.com.treinaweb.twclientes.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +15,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(ModelMap model) {
+        model.addAttribute("boasVindas", "Bem vindo ao curso de Spring com Thymeleaf");
+
+        List<String> aulas = new ArrayList<>();
+        aulas.add("Aula 01 - Introdução");
+        aulas.add("Aula 02 - Template engines");
+        aulas.add("Aula 03 - Arquivos estáticos");
+
+        model.addAttribute("aulas", aulas);
+
         return "home";
     }
 
