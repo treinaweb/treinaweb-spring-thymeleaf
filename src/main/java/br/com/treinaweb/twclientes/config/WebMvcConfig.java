cb2542143b9426f24e4b693411bbd544b3a1d500
Cleyson Lima <cleysonph@gmail.com>
2021-03-11 22:29:28 -0300

    08-16: configurando o LocaleResolver do Spring Web MVC

---
+++ src/main/java/br/com/treinaweb/twclientes/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package br.com.treinaweb.twclientes.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("pt"));
+
+        return slr;
+    }
+
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    
+}
